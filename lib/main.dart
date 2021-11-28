d7c8d320242ea8a08c873611d3a3f668162ab76d
youcova428 <y.k.0428.777@gmail.com>
2021-11-28 21:00:36 +0900

    イベントカレンダーメイン画面に表示、カレンダー3年表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medicine_reminder/calender_page.dart';
+import 'package:medicine_reminder/table_event_example.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CalenderPage(),
+      home: TableEventsExample(),
     );
   }
 }
